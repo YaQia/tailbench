12d8d95a0b688b61e31c20cad4db3cc705d42cc5
姚益祁 <61108050+YaQia@users.noreply.github.com>
2025-03-29 08:28:15 +0000

    add auto-run.sh support for tailbench apps.

--- img-dnn/run.sh
+++ img-dnn/run.sh
@@ -5,8 +5,9 @@ source ${DIR}/../configs.sh
 
 THREADS=1
 REQS=100000000 # Set this very high; the harness controls maxreqs
+QPS=${QPS:-500}
 
-TBENCH_WARMUPREQS=5000 TBENCH_MAXREQS=10000 TBENCH_QPS=500 \
+TBENCH_WARMUPREQS=5000 TBENCH_MAXREQS=10000 TBENCH_QPS=${QPS} \
     TBENCH_MINSLEEPNS=10000 TBENCH_MNIST_DIR=${DATA_ROOT}/img-dnn/mnist \
     ./img-dnn_integrated -r ${THREADS} \
     -f ${DATA_ROOT}/img-dnn/models/model.xml -n ${REQS}
--- shore/run.sh
+++ shore/run.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/../configs.sh
 
 THREADS=1
-QPS=400
+QPS=${QPS:-400}
 WARMUPREQS=10000
 MAXREQS=2000
 
